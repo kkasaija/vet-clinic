84a95818b4fe4b583f245a30b180901fcddb0191
Kasaija Kenneth <kasaijak@gmail.com>
2022-12-12 20:26:05 +0300

    Querry Data

--- data.sql
+++ data.sql
@@ -1,14 +1,14 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (1, 'Agumon', '2020-03-02', 0, true, 10.23);
+VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (2, 'Gabumon', '2018-15-11', 2, true, 8);
+VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (3, 'Pikachu', '2021-07-01', 1, false, 15.04);
+VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (4, 'Devimon', '2017-12-05', 5, true, 15.04);
+VALUES (4, 'Devimon', '2017-05-12', 5, true, 15.04);
 
--- schema.sql
+++ schema.sql
@@ -2,10 +2,11 @@
 
 CREATE DATABASE vet_clinic;
 CREATE TABLE animals (
-    id int PRIMARY KEY,
+    id int,
     name text,
     date_of_birth date,
     escape_attempts int,
     neutered boolean,
-    weight_kg float
+    weight_kg float,
+    PRIMARY KEY (id)
 );
