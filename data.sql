cb1133ac31cc0da822f0636f2e342617e76962d4
Kasaija Kenneth <kasaijak@gmail.com>
2022-12-15 00:23:01 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -22,3 +22,32 @@ VALUES ('Charmander', '2020-02-08', 0, false, -11, 'Animal'),
 ('Boarmon', '2005-06-07', 7, true, 20.4, 'Animal'),
 ('Blossom', '1998-06-13', 3, true, 17, 'Animal'),
 ('Ditto', '2022-05-14', 4, true, 22, 'Animal');
+
+/*Project three*/
+/*Owners table*/
+
+INSERT INTO owners(full_name,age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+/*Species table*/
+
+INSERT INTO species(name) 
+VALUES('Pokemon'),('Digimon');
+
+/*Update species in animals table*/
+
+UPDATE animals SET species_id=2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id=1 WHERE species_id IS NULL;
+
+/*Update owners in animals table*/
+
+UPDATE animals SET owners_id = 1 WHERE name='Agumon';
+UPDATE animals SET owners_id = 2 WHERE name='Gabumon' OR name='Pikachu';
+UPDATE animals SET owners_id = 3 WHERE name='Devimon' OR name='Plantmon';
+UPDATE animals SET owners_id = 4 WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
+UPDATE animals SET owners_id = 5 WHERE name='Angemon' OR name='Boarmon';
