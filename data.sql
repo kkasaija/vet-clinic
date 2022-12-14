c072e6cbbdfc8d54e0273c4e9694b50cafdec9af
Kasaija Kenneth <kasaijak@gmail.com>
2022-12-14 03:48:38 +0300

    Insert new records

--- data.sql
+++ data.sql
@@ -12,3 +12,13 @@ VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (4, 'Devimon', '2017-05-12', 5, true, 15.04);
 
+/*newly inserted records*/
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES (5, 'Charmander', '2020-02-08', 0, false, 11, 'Animal'),
+(6, 'Plantmon', '2021-11-15', 2, true, 5.7, 'Animal'),
+(7, 'Squirtle', '1993-04-02', 3, false, 12.13, 'Animal'),
+(8, 'Angemon', '2005-06-12', 1, true, 45, 'Animal'),
+(9, 'Boarmon', '2005-06-07', 7, true, 20.4, 'Animal'),
+(10, 'Blossom', '1998-06-13', 3, true, 17, 'Animal'),
+(11, 'Ditto', '2022-05-14', 4, true, 22, 'Animal');
