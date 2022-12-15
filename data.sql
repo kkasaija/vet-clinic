35080b21b633e4f582a99dc605fa414be39bcf91
Kasaija Kenneth <kasaijak@gmail.com>
2022-12-16 00:25:41 +0300

    Insert vets data

--- data.sql
+++ data.sql
@@ -51,3 +51,12 @@ UPDATE animals SET owners_id = 2 WHERE name='Gabumon' OR name='Pikachu';
 UPDATE animals SET owners_id = 3 WHERE name='Devimon' OR name='Plantmon';
 UPDATE animals SET owners_id = 4 WHERE name='Charmander' OR name='Squirtle' OR name='Blossom';
 UPDATE animals SET owners_id = 5 WHERE name='Angemon' OR name='Boarmon';
+
+-----------------------------------------------------------------------
+/*PROJECT FOUR*/
+
+INSERT INTO vets(name,age,date_of_graduation)
+VALUES('William Thatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
