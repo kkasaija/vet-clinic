6c69689c75c9f218c9b3955b6b7994ec1a0b5ef7
Kasaija Kenneth <kasaijak@gmail.com>
2022-12-15 21:34:12 +0300

    Add vets, specialization, and visits tables

--- schema.sql
+++ schema.sql
@@ -13,12 +13,12 @@ CREATE TABLE animals (
     PRIMARY KEY (id)
 );
 
-/*Project two*/
+/*PROJECT TWO*/
 
 ALTER TABLE animals 
 ADD COLUMN species VARCHAR(255);
 
-/*Project three*/
+/*PROJECT THREE*/
 
 CREATE TABLE owners(
   id SERIAL PRIMARY KEY,
@@ -36,3 +36,31 @@ ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owners_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_animals_owners FOREIGN KEY (owners_id) REFERENCES owners(id);
+
+-----------------------------------------------------------------------
+
+/*PROJECT FOUR*/
+
+CREATE TABLE vets(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  age INT NOT NULL,
+  date_of_graduation date NOT NULL
+);
+
+CREATE TABLE specializations(
+  species_id INT,
+  vets_id INT,
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT fk_species_vets PRIMARY KEY (species_id, vets_id)
+);
+
+CREATE TABLE visits(
+  id SERIAL PRIMARY KEY,
+  animals_id INT,
+  vets_id INT,
+  date_of_visit date NOT NULL,
+  CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id) ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
